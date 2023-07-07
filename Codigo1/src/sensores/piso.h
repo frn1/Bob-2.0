1fb9baeec7dacdc405e5839e3e7d10637b63c4b2
Francisco José Torres Contarini <20214662+frn1@users.noreply.github.com>
2023-07-07 13:56:49 -0300

    dumb mistake :/

--- Codigo1/src/sensores/piso.h
+++ Codigo1/src/sensores/piso.h
@@ -12,7 +12,8 @@ extern Smoothed<uint16_t> lecturasPiso[N_PISO];
 inline void setupPiso(uint8_t n_sensor, uint8_t pin)
 {
   pinMode(pin, INPUT);
-  lecturasPiso[n_sensor] = Smoothed<uint16_t>(SMOOTHED_AVERAGE, 4);
+  lecturasPiso[n_sensor] = Smoothed<uint16_t>;
+  lecturasPiso[n_sensor].begin(SMOOTHED_AVERAGE, 4);
 }
 
 // Setup todos los sensores del piso
