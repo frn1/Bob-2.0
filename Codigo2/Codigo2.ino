d6818793a909aa19f5df1db4749b070b520447dd
frn1 <torrescontarini@gmail.com>
2023-09-01 14:47:40 -0300

    Añadir subódulo de gcem y usar calculo para presión del aire y velocidad de sonido inicial

--- Codigo2/Codigo2.ino
+++ Codigo2/Codigo2.ino
@@ -1,3 +1,5 @@
+#include "src/gcem/include/gcem.hpp"
+
 constexpr uint8_t MTR_L_ADELANTE = A3;
 constexpr uint8_t MTR_L_ATRAS = A0;
 constexpr uint8_t MTR_R_ADELANTE = A1;
@@ -11,6 +13,39 @@ constexpr uint8_t TATAMI_R = A10;
 constexpr uint8_t BOT_COMIENZO = 12;
 
 constexpr uint8_t LED = LED_BUILTIN;
+
+// Gas density in kg/m^3 (Kilogram per meter cubed)
+constexpr float DENSIDAD_AIRE = 1.225;
+// Molar mass of air in kg/mol (Kilograms per mole)
+constexpr float MASA_MOLAR_AIRE = 28.9644 / 1000;
+// Gravity   in m/s^2 (Meters per second squared)
+constexpr float GRAVEDAD = 9.79701;
+// The ALTITUD the robots currently at in m (Meters)
+constexpr float ALTITUD = 77;
+// The ALTITUD where the reference was measured in m (Meters)
+constexpr float ALTITUD_REFERENCIA = 0;
+// The current temperature the robots is currently in K (Kelvins)
+constexpr float TEMPERATURE = 283.2;  // 10 °C
+// The temperature at the place the reference was measured in K (Kelvins)
+constexpr float TEMPERATURE_REFERENCE = 287.15;  // 14 °C
+// The Universal Gas Constant in J/(mol K) (Joules per mole kelvin)
+constexpr float UNIVERSAL_GAS_CONSTANT = 0.00831446261815324;
+// Gas pressure at the place where the reference was measured in Pa (Pascals)
+constexpr float PRESSURE_REFERENCE = 101325;
+// Gas pressure at current height in Pa (Pascals)
+constexpr float PRESSURE = PRESSURE_REFERENCE * gcem::exp(-GRAVEDAD * MASA_MOLAR_AIRE * (ALTITUD - ALTITUD_REFERENCIA) / (UNIVERSAL_GAS_CONSTANT * TEMPERATURE_REFERENCE));
+// Gas adibatic index (7/5 for diatomic, 5/3 for monatomic)
+constexpr float ADIBATIC_INDEX = 7.0 / 5.0;
+// The speed of sound in mm/us (millimeters per microseconds)
+constexpr float SPEED_OF_SOUND = gcem::sqrt((ADIBATIC_INDEX * PRESSURE) / DENSIDAD_AIRE) / 1000;
+// Maximum distance to read in mm (millimeters)
+constexpr float MAX_DIST = 700;
+// Time it takes for sound to move 2 mm (millimeters)
+constexpr unsigned long TIME_PER_MM = 2.0 / SPEED_OF_SOUND;
+// Timeout for a reading in us (microseconds)
+constexpr unsigned long TIMEOUT_DIST = MAX_DIST * TIME_PER_MM * 3;
+// Time between readings in ms (milliseconds)
+constexpr unsigned long TIME_PER_READING_DIST = TIMEOUT_DIST / 1000 + 1;
 
 void setup() {
   pinMode(MTR_L_ADELANTE, OUTPUT);
@@ -44,8 +79,8 @@ long ultra(uint8_t trig, uint8_t echo) {
   delayMicroseconds(10);  //Enviamos un pulso de 10us
   digitalWrite(trig, LOW);
 
-  t = pulseIn(echo, HIGH, 5000);  //obtenemos el ancho del pulso
-  d = t / 59;                     //escalamos el tiempo a una distancia en cm
+  t = pulseIn(echo, HIGH, TIMEOUT_DIST);  //obtenemos el ancho del pulso
+  d = t / TIME_PER_MM;                     //escalamos el tiempo a una distancia en cm
   return d;
 }
 
