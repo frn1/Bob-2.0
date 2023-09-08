3969e8aa4eead13eef0187e27e1bccf39a372a06
Francisco José Torres Contarini <torrescontarini@gmail.com>
2023-09-08 14:30:36 -0300

    Limpiar un poco el código

--- Codigo2/Codigo2.ino
+++ Codigo2/Codigo2.ino
@@ -1,65 +1,109 @@
+/***MOTORES***/
+//Inicializacion de pines
+
+// Velocidad de los motores
+//! MINIMO 130!!! SI NO NO SE MUEVE!!!
+constexpr uint8_t VELOCIDAD = 130;
+
+// Motor Izq. Adelante
 constexpr uint8_t MTR_L_ADELANTE = A3;
+// Motor Izq. Atras
 constexpr uint8_t MTR_L_ATRAS = A0;
+// Motor Der. Adelante
 constexpr uint8_t MTR_R_ADELANTE = A1;
+// Motor Der. Atras
 constexpr uint8_t MTR_R_ATRAS = A2;
 
+// Mover Adelante
+void adelante() {
+  analogWrite(MTR_L_ADELANTE, VELOCIDAD);
+  analogWrite(MTR_R_ADELANTE, VELOCIDAD);
+  analogWrite(MTR_L_ATRAS, 0);
+  analogWrite(MTR_R_ATRAS, 0);
+}
+
+// Girar Izq.
+void izquierda() {
+  analogWrite(MTR_L_ADELANTE, VELOCIDAD);
+  analogWrite(MTR_R_ADELANTE, 0);
+  analogWrite(MTR_L_ATRAS, 0);
+  analogWrite(MTR_R_ATRAS, 0);
+}
+
+// Girar Der.
+void derecha() {
+  analogWrite(MTR_L_ADELANTE, 0);
+  analogWrite(MTR_R_ADELANTE, VELOCIDAD);
+  analogWrite(MTR_L_ATRAS, 0);
+  analogWrite(MTR_R_ATRAS, 0);
+}
+
+// Mover Atras
+void atras() {
+  analogWrite(MTR_L_ADELANTE, 0);
+  analogWrite(MTR_R_ADELANTE, 0);
+  analogWrite(MTR_L_ATRAS, VELOCIDAD);
+  analogWrite(MTR_R_ATRAS, VELOCIDAD);
+}
+
+// Parar
+void parar() {
+  analogWrite(MTR_L_ADELANTE, 0);
+  analogWrite(MTR_R_ADELANTE, 0);
+  analogWrite(MTR_L_ATRAS, 0);
+  analogWrite(MTR_R_ATRAS, 0);
+}
+
+// Setup los motores
+void setupMotor() {
+  pinMode(MTR_L_ADELANTE, OUTPUT);
+  pinMode(MTR_L_ATRAS, OUTPUT);
+  pinMode(MTR_R_ADELANTE, OUTPUT);
+  pinMode(MTR_R_ATRAS, OUTPUT);
+
+  parar();
+}
+
+/*****ULTRASONICOS****/
+
 constexpr uint8_t TRIG = 3;
-constexpr uint8_t ECHO = 6;
-
-constexpr uint8_t TATAMI_L = A9;
-constexpr uint8_t TATAMI_R = A10;
-constexpr uint8_t BOT_COMIENZO = 12;
-
-constexpr uint8_t LED = LED_BUILTIN;
-
-constexpr uint8_t VELOCIDAD = 130; // MINIMO 130!!! SI NO NO SE MUEVE!!!
+constexpr uint8_t ECHO_L = 4;
+constexpr uint8_t ECHO_C = 5;
+constexpr uint8_t ECHO_R = 6;
 
 // La velocidad del sonido en cm/us (centimetros por microsegundos)
 constexpr float VELOCIDAD_DEL_SONIDO = 0.34027 / 10;
 // Maxima distancia a leer en cm (centimetros)
 constexpr float MAX_DIST = 25;
 // Tiempo que tarda el sonido en recorer 2 cm (centimetros)
-constexpr unsigned long TIEMPO_PARA_2CM = (unsigned long)((2.0 / VELOCIDAD_DEL_SONIDO) * 10) % 10 >= 5 ? 2.0 / VELOCIDAD_DEL_SONIDO + 1 : 2.0 / VELOCIDAD_DEL_SONIDO;  // Round the number
+constexpr unsigned long TIEMPO_PARA_2CM = (unsigned long)((2.0 / VELOCIDAD_DEL_SONIDO) * 10) % 10 >= 5 ? 2.0 / VELOCIDAD_DEL_SONIDO + 1 : 2.0 / VELOCIDAD_DEL_SONIDO;  // Redondeo
 // Tiempo máximo para leer en us (microsegundos)
 constexpr unsigned long TIEMPO_MAX_LECTURA = MAX_DIST * TIEMPO_PARA_2CM * 2;
 // Tiempo entre lecturas en ms (milisegundos)
 constexpr unsigned long TIEMPO_POR_LECTURA = TIEMPO_MAX_LECTURA / 1000 + 3;
 
-unsigned long ultima_dist;
+// Ultimo tiempo de medicion de los ultrasonicos
+unsigned long ultima_medicion_dist;
+// Distancia del ultrasónico de la izquierda
+unsigned long izq;
+// Distancia del ultrasónico del centro
+unsigned long cen;
+// Distancia del ultrasónico de la derecha
+unsigned long der;
 
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(MTR_L_ADELANTE, OUTPUT);
-  pinMode(MTR_L_ATRAS, OUTPUT);
-  pinMode(MTR_R_ADELANTE, OUTPUT);
-  pinMode(MTR_R_ATRAS, OUTPUT);
+// Setup ultrasonicos
+void setupUltra() {
   pinMode(TRIG, OUTPUT);
-  pinMode(TATAMI_L, INPUT);
-  pinMode(TATAMI_R, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-  pinMode(LED, OUTPUT);
-  pinMode(BOT_COMIENZO, INPUT_PULLUP);
+  pinMode(ECHO_L, INPUT);
+  pinMode(ECHO_C, INPUT);
+  pinMode(ECHO_R, INPUT);
 
-  Serial.println("Vamos en 5!");
-  for (uint8_t i = 0; i < 5; i++) {
-    Serial.print(5 - i);
-    Serial.println("...");
-    digitalWrite(LED, HIGH);
-    delay(500);
-    digitalWrite(LED, LOW);
-    delay(500);
-  }
-
-  analogWrite(MTR_L_ADELANTE, 0);
-  analogWrite(MTR_R_ADELANTE, VELOCIDAD);
-  analogWrite(MTR_L_ATRAS, 0);
-  analogWrite(MTR_R_ATRAS, 0);
-  ultima_dist = millis();
+  izq = ultra(TRIG, ECHO_L);
+  cen = ultra(TRIG, ECHO_C);
+  der = ultra(TRIG, ECHO_R);
 }
 
+// Medir distancia del ultrasonido
 unsigned long ultra(uint8_t trig, uint8_t echo) {
   unsigned long t;  //timepo que demora en llegar el eco
   unsigned long d;  //distancia en centimetros
@@ -73,11 +117,56 @@ unsigned long ultra(uint8_t trig, uint8_t echo) {
   return d;
 }
 
-void loop() {
-  Serial.println(analogRead(TATAMI_R));
-  Serial.println(analogRead(TATAMI_L));
+/***SENSORES DE PISO***/
 
-  if (analogRead(TATAMI_L) < 450) {
+// CNY de piso izquierdo
+constexpr uint8_t CNY_L = A9;
+// CNY de piso derecho
+constexpr uint8_t CNY_R = A10;
+
+// Setup sensores de piso
+void setupPiso() {
+  pinMode(CNY_L, INPUT);
+  pinMode(CNY_R, INPUT);
+}
+
+/***MISC.***/
+
+// Boton para inciar el código
+// No está implementado por ahora
+constexpr uint8_t BOT_COMIENZO = 12;
+// Led para lo que quieras
+constexpr uint8_t LED = 7;
+
+void setup() {
+  Serial.begin(115200);
+  setupMotor();
+  setupUltra();
+  setupPiso();
+  pinMode(LED, OUTPUT);
+  pinMode(BOT_COMIENZO, INPUT_PULLUP);
+
+  // Esperar 5 segundos antes de prender el robot
+  // Serial.println("Vamos en 5!");
+  // for (uint8_t i = 0; i < 5; i++) {
+  //   Serial.print(5 - i);
+  //   Serial.println("...");
+  //   // Prender y apagar led
+  //   digitalWrite(LED, HIGH);
+  //   delay(500);
+  //   digitalWrite(LED, LOW);
+  //   delay(500);
+  // }
+  delay(5000);
+
+  // Inicializar timer de los ultrasonidos
+  ultima_medicion_dist = millis();
+}
+
+// Código para evitar que se caiga
+// TODO: Hacerlo más lindo
+void evitarBlanco() {
+  if (analogRead(CNY_L) < 450) {
     analogWrite(MTR_L_ADELANTE, 0);
     analogWrite(MTR_R_ADELANTE, 0);
     analogWrite(MTR_L_ATRAS, 0);
@@ -91,8 +180,7 @@ void loop() {
     analogWrite(MTR_L_ATRAS, 0);
     analogWrite(MTR_R_ATRAS, 0);
     delay(10);
-  }
-  if (analogRead(TATAMI_R) < 450) {
+  } else if (analogRead(CNY_R) < 450) {
     analogWrite(MTR_L_ADELANTE, 0);
     analogWrite(MTR_R_ADELANTE, 0);
     analogWrite(MTR_L_ATRAS, 0);
@@ -106,7 +194,7 @@ void loop() {
     analogWrite(MTR_L_ATRAS, 0);
     analogWrite(MTR_R_ATRAS, 0);
     delay(10);
-  } else if (analogRead(TATAMI_R) < 450 || analogRead(TATAMI_L) < 450) {
+  } else if (analogRead(CNY_R) < 450 || analogRead(CNY_L) < 450) {
     analogWrite(MTR_L_ADELANTE, 0);
     analogWrite(MTR_R_ADELANTE, 0);
     analogWrite(MTR_L_ATRAS, 0);
@@ -121,59 +209,40 @@ void loop() {
     analogWrite(MTR_R_ATRAS, 0);
     delay(10);
   }
-  static unsigned long izq = ultra(TRIG, 4);
-  static unsigned long cen = ultra(TRIG, 5);
-  static unsigned long der = ultra(TRIG, 6);
-  if (millis() - ultima_dist > TIEMPO_POR_LECTURA) {
-    izq = ultra(TRIG, 4);
-    cen = ultra(TRIG, 5);
-    der = ultra(TRIG, 6);
-    ultima_dist = millis();
+}
+
+void loop() {
+  Serial.println(analogRead(CNY_R));
+  Serial.println(analogRead(CNY_L));
+
+  evitarBlanco();
+
+  if (millis() - ultima_medicion_dist > TIEMPO_POR_LECTURA) {
+    izq = ultra(TRIG, ECHO_L);
+    cen = ultra(TRIG, ECHO_C);
+    der = ultra(TRIG, ECHO_R);
+    ultima_medicion_dist = millis();
   }
 
   bool girando = false;
 
   if (cen != 0) {
     Serial.println("Adelante");
-    analogWrite(MTR_L_ADELANTE, VELOCIDAD);
-    analogWrite(MTR_R_ADELANTE, VELOCIDAD);
-    analogWrite(MTR_L_ATRAS, 0);
-    analogWrite(MTR_R_ATRAS, 0);
+    adelante();
     girando = false;
   } else if (izq != 0) {
     Serial.println("Izquierda");
-    analogWrite(MTR_L_ADELANTE, VELOCIDAD);
-    analogWrite(MTR_R_ADELANTE, 0);
-    analogWrite(MTR_L_ATRAS, 0);
-    analogWrite(MTR_R_ATRAS, 0);
+    izquierda();
     girando = true;
   } else if (der != 0) {
     Serial.println("Derecha");
-    analogWrite(MTR_L_ADELANTE, 0);
-    analogWrite(MTR_R_ADELANTE, VELOCIDAD);
-    analogWrite(MTR_L_ATRAS, 0);
-    analogWrite(MTR_R_ATRAS, 0);
+    derecha();
     girando = true;
   } else {
     Serial.println("Ninguno");
     if (girando == false) {
-      analogWrite(MTR_L_ADELANTE, 0);
-      analogWrite(MTR_R_ADELANTE, VELOCIDAD);
-      analogWrite(MTR_L_ATRAS, 0);
-      analogWrite(MTR_R_ATRAS, 0);
+      derecha();
       girando = true;
     }
   }
-  // if (ult != 0 && ult <= 60) {
-
-  //   analogWrite(MTR_L_ADELANTE, 255);
-  //   analogWrite(MTR_R_ADELANTE, 255);
-  //   analogWrite(MTR_L_ATRAS, 0);
-  //   analogWrite(MTR_R_ATRAS, 0);
-  // } else {
-  //   analogWrite(MTR_L_ADELANTE, 255);
-  //   analogWrite(MTR_R_ADELANTE, 0);
-  //   analogWrite(MTR_L_ATRAS, 0);
-  //   analogWrite(MTR_R_ATRAS, 0);
-  // }
 }
